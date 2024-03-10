21251f8425b22d07090dfe4efd8f84a63bb9f15e
cam <cameronmcorist@gmail.com>
2024-03-10 11:21:47 +1100

    fix

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,5 +1,7 @@
-const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
@@ -11,9 +13,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>
-          {animal} - {breed} - {location}
-        </h2>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
     </a>
   );
